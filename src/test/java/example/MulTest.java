f5d7b8e32665e0c4d84727af51e8dd98c4a25d20
Ryosuke Yabuki <ryosuke.yabuki+gh@gmail.com>
2026-05-27 12:26:51 +0900

    fix

--- src/test/java/example/MulTest.java
+++ src/test/java/example/MulTest.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.*;
 public class MulTest {
     @Test
     public void calc() {
-        assertThat(new Mul().calc(8,3), is(16));
+        assertThat(new Mul().calc(8,3), is(24));
     }
 }
